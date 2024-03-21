e4932145554db52853d20bb309e51ce75ba990e6
wkromrei <wkromrei@gmail.com>
2024-03-20 21:25:18 -0500

    finished all the elements of the assignment, aside from README.

--- js/blog.js
+++ js/blog.js
@@ -1,4 +1,6 @@
-
+const backButtonEl = document.getElementById('backButtonElement');
+const changeLightEl = document.getElementById('changeLightElement')
+const body = document.querySelector('body');
 // Retrieve data from local storage
 const blogs = JSON.parse(localStorage.getItem('blogs')) || [];
 
@@ -10,16 +12,29 @@ const postsContainer = document.getElementById('posts-container');
     for (let i = 0; i < blogs.length; i++) {
         const post = blogs[i];
 
-
     const postElement = document.createElement('div');
     postElement.innerHTML = `
-        <p>${post.username}</p>
-        <p>${post.title}</p>
-        <p>${post.content}</p>
+        <h2>${post.title}</h2>
+        <h3>${post.username}</h3>
+        <textbox>${post.content}</textbox>
     `;
     postsContainer.appendChild(postElement);
 };
 
 
+let isDark = true;
 
-
+changeLightEl.addEventListener('click', function() {
+    if (isDark) {
+        body.style.backgroundColor = '#d9e9e8';
+        body.style.color = '#1a1a1a';
+        isDark = !isDark;
+    } else {
+        body.style.backgroundColor = '#1a1a1a';
+        body.style.color = '#d9e9e8';
+        isDark = !isDark;
+    }
+});
+backButtonEl.addEventListener('click', function() {
+    location.pathname='./index.html'
+})
